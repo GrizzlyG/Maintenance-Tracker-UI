152078fa603bc874ca3925aa990b91375c87aae9
Grizzly <m4botdev@gmail>
2018-05-11 21:12:07 +0100

    Added links to all pages

---
+++ main.js
@@ -0,0 +1,22 @@
+window.onload = function(){
+    var mRequest = document.querySelector("#mtn-btn");
+    var rRequest = document.querySelector("#rep-btn");
+    var display = document.querySelector(".display");
+    var mtext = document.querySelector("#mtn-txt");
+    var rtext = document.querySelector("#rep-txt");
+
+    rRequest.addEventListener("click",rCast,false);
+    mRequest.addEventListener("click",mCast,false);
+
+    function rCast(){
+        var message;
+        message = rtext.value+" (repair request)";
+        display.innerHTML = message;
+    }
+
+    function mCast(){
+        var message;
+        message = mtext.value+" (maintenance request)";
+        display.innerHTML = message;
+    }
+}
